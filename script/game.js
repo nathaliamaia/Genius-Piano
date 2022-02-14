205e94fe84bff5528409c3258644a720e9a48389
Nathalia Maia <nathaliamaiape@gmail.com>
2022-02-13 23:06:31 -0300

    initial of js code

--- script/game.js
+++ script/game.js
@@ -1,1 +1,10 @@
+// 1. sortear aleatoriamente valores entre a primeira e ultima tecla do piano. 
+// 2. salvar o valor sorteado dentro de uma array vazia.
+// 3. mostrar a tecla (através de um toque musical  e cor diferente da tecla) selecionada para que o player possa visualizá-la .
+
+//4. Após o clique do player, salvar o valor da iteração em uma array vazia.
+//5. Comparar o valor da array da escolha do player com a array da tecla sorteada.
+// 6. Se o player acertar a sequencia, o loop recomeça com uma tecla a mais.
+// 7. Se o player errar a tecla, aparece uma mensagem "você perdeu".
 // 
+
--- script/index.js
+++ script/index.js
@@ -1,3 +1,18 @@
 const BLACK_KEYS = [];
 const WHITE_KEYS = [];
 
+const keys = document.querySelectorAll(".key");
+ 
+keys.forEach(key => {
+  key.addEventListener("click", () => playNote(key)) 
+})
+
+function playNote(key) {
+  const noteAudio = document.getElementById(key.dataset.note)
+  noteAudio.currentTime = 0
+  noteAudio.play();
+  key.classList.add('active');
+  noteAudio.addEventListener('ended', () => {
+      key.classList.remove('active')
+  })
+}
