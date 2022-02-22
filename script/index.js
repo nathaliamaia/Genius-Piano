61c2e27849a6d60a5085514ae8bb4be5fdb1bfc2
Nathalia Maia <nathaliamaiape@gmail.com>
2022-02-22 19:30:40 -0300

    added new functions

--- script/index.js
+++ script/index.js
@@ -5,23 +5,20 @@ const keys = document.querySelectorAll(".key");
 const playerScore = document.getElementById("counter");
 const powerBtn = document.getElementById("powerButton");
 
-
 let cpuSequence = [];
 let playerSequence = [];
 
 let count = 1;
-let level = 0;
-
-
+let power = "off";
 
 keys.forEach((key) => {
   key.addEventListener("click", () => playNote(key));
   key.addEventListener("click", () => key.classList.add("click"));
-  key.addEventListener("click", () => { 
-    if(mode === "Game")
-      handleChoise(key) 
-  }); 
+  key.addEventListener("click", () => {
+    if (mode === "Game") handleChoise(key);
+  });
   key.addEventListener("mouseout", () => key.classList.remove("click"));
+  key.addEventListener("mouseout", () => key.classList.remove("active"));
 });
 
 // audio das keys
@@ -50,8 +47,7 @@ function pickRandomKey() {
 }
 
 function nextRound() {
-  level += 1;
-  const nextSequence = [...cpuSequence]; 
+  const nextSequence = [...cpuSequence];
   nextSequence.push(pickRandomKey());
   cpuSequence = nextSequence;
   playRound(nextSequence);
@@ -61,43 +57,50 @@ function nextRound() {
 function playGame() {
   //sorteia a key e coloca na array da cpu
   nextRound();
+  startBtn.classList.add("unclickable");
 }
 
 //botao de start
-startBtn.addEventListener("click", () => {
-  mode = "Game";
-  playerScore.innerHTML = "0";
-  playGame();
-});
+// startBtn.addEventListener("click", () => {
+//   mode = "Game";
+//   playerScore.innerHTML = "0";
+//   playGame();
+// });
 
 //botão para "ligar"o jogo.
 powerBtn.addEventListener("click", (event) => {
   playerScore.innerHTML = "--";
+  if ((power = "on")) {
+    startBtn.addEventListener("click", () => {
+      mode = "Game";
+      playerScore.innerHTML = "0";
+      playGame();
+    });
+  }
+  
 });
 
 // escolha do jogador
 function handleChoise(key) {
   const index = playerSequence.push(key) - 1;
-  
-  // Checa sequência 
-  if(playerSequence[index] !== cpuSequence[index]) {
+
+  // Checa sequência
+  if (playerSequence[index] !== cpuSequence[index]) {
     alert("Você perdeu");
     resetGame();
     return;
   } else if (playerSequence[index] == cpuSequence[index]) {
-    // playerScore.innerHTML = count;
     updatePlayerScore();
-    // count++; //rever
   }
 
-  if(playerSequence.length === cpuSequence.length) {
+  if (playerSequence.length === cpuSequence.length) {
     playerSequence = [];
-    
+
     // alert("Você acertou!");
     setTimeout(() => {
       nextRound();
     }, 1000);
-    
+
     return;
   }
 }
@@ -105,22 +108,22 @@ function updatePlayerScore() {
   playerScore.innerHTML = count;
   count++;
 }
-function resetGame(){
+
+function resetGame() {
   cpuSequence = [];
   playerSequence = [];
-  level = 0;
   count = 0;
+  playerScore.innerHTML = "--";
 }
-
+restartBtn.addEventListener("click", () => {
+  resetGame();
+});
 
 // TO DO:
 
 //1.score OK
-// 
-//2. permitir o  modo livre ("brincar"com o piano livremente)-> se tiver nesse modo, não entra no handlechoise  OK
-//3. se der tempo, adicionar opcao de usar teclas para brincar com o piano.
-//4. restart.
-//5. depois que errar, só pode apertar no restart
-//6. Não poder clicar no start enquanto tiver na partida
-//7. rever tempo de efeito do "active"
-//8. Só deixar clicar depois que a cpu gera uma nova key na sequencia.
+// 2. esperar o computador gerar a sequencia, para permitir o clique;
+//3.. permitir o  modo livre ("brincar"com o piano livremente)-> se tiver nesse modo, não entra no handlechoise  OK
+//4. se der tempo, adicionar opcao de usar teclas para brincar com o piano.
+//5. Não poder clicar no start enquanto tiver na partida
+//6. rever tempo de efeito do "active"
